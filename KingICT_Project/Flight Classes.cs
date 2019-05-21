8a0014c8c2f7a4400b65aec2deac12d7df83e28d
DomagojHamzic <domagoj.hamzic@gg-grupa.eu>
2019-05-21 11:33:39 +0200

    Classes for receiving data from Amadeus Web Service are added.

---
+++ KingICT_Project/Flight Classes.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingICT_Project
+{
+
+    public class Rootobject
+    {
+        public Datum[] data { get; set; }
+        public Dictionaries dictionaries { get; set; }
+        public Meta meta { get; set; }
+        public Warning[] warnings { get; set; }
+    }
+
+    public class Dictionaries
+    {
+        public Currencies currencies { get; set; }
+        public Locations locations { get; set; }
+    }
+
+    public class Currencies
+    {
+        public string EUR { get; set; }
+    }
+
+    public class Locations
+    {
+        public MAD MAD { get; set; }
+        public MUC MUC { get; set; }
+    }
+
+    public class MAD
+    {
+        public string subType { get; set; }
+        public string detailedName { get; set; }
+    }
+
+    public class MUC
+    {
+        public string subType { get; set; }
+        public string detailedName { get; set; }
+    }
+
+    public class Meta
+    {
+        public string currency { get; set; }
+        public Links links { get; set; }
+        public Defaults defaults { get; set; }
+    }
+
+    public class Links
+    {
+        public string self { get; set; }
+    }
+
+    public class Defaults
+    {
+        public string departureDate { get; set; }
+        public bool oneWay { get; set; }
+        public string duration { get; set; }
+        public bool nonStop { get; set; }
+        public string viewBy { get; set; }
+    }
+
+    public class Datum
+    {
+        public string type { get; set; }
+        public string origin { get; set; }
+        public string destination { get; set; }
+        public string departureDate { get; set; }
+        public string returnDate { get; set; }
+        public Price price { get; set; }
+        public Links1 links { get; set; }
+    }
+
+    public class Price
+    {
+        public string total { get; set; }
+    }
+
+    public class Links1
+    {
+        public string flightDestinations { get; set; }
+        public string flightOffers { get; set; }
+    }
+
+    public class Warning
+    {
+        public string title { get; set; }
+    }
+
+}
--- KingICT_Project/Form1.Designer.cs
+++ KingICT_Project/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace KingICT_Project
 {
-    partial class Form1
+    partial class UiMainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // UiMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UiMainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cheapest Flights";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
